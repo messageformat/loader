fc9f8c54ffddbb23608ac4ccc7b203f6754f1d65
Eemeli Aro <eemeli@gmail.com>
2017-07-14 20:34:41 +0300

    Rewrite for WebPack loader API v2, updating dependencies

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 var loaderUtils = require('loader-utils');
 var MessageFormat = require('messageformat');
 
-module.exports = function(content) {
-  var query = loaderUtils.parseQuery(this.query);
-  var locale = query.locale || 'en';
-  var messages = (Array.isArray(this.inputValue) && typeof this.inputValue[0] === 'object') ? this.inputValue[0] : this.exec(content, this.resource);
-  var messageFormat = new MessageFormat(locale);
-  if (query.disablePluralKeyChecks) {
-    messageFormat.disablePluralKeyChecks();
-  }
-  if (query.intlSupport) {
-    messageFormat.setIntlSupport(true);
-  }
-  var messageFunctions = messageFormat.compile(messages);
-
-  this.cacheable && this.cacheable();
-  this.value = [ messageFunctions ];
-
-  return messageFunctions.toString('module.exports');
-};
+module.exports = function(messages) {
+  var options = loaderUtils.getOptions(this);
+  let lc = options.locale;
+  if (typeof lc === 'string' && lc.indexOf(',') !== -1) lc = lc.split(',');
+  var mf = new MessageFormat(lc);
+  if (options.biDiSupport) mf.setBiDiSupport();
+  if (options.disablePluralKeyChecks) mf.disablePluralKeyChecks();
+  if (options.formatters) mf.addFormatters(options.formatters);
+  if (options.intlSupport) mf.setIntlSupport(true);
+  if (options.strictNumberSign) mf.setStrictNumberSign();
+  return mf.compile(JSON.parse(messages)).toString('module.exports');
+}
